56f3aaafc5c28377cdfa8184647e017c20dc5dd5
Travis Luong <travis@travisluong.com>
2022-10-13 00:41:25 -0700

    ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [{
+    name: "app",
+    script: 'server.js',
+  }],
+
+  // Deployment Configuration
+  deploy: {
+    production: {
+      "key": "key.pem",
+      "user": "ubuntu",
+      "host": ["54.187.13.4"],
+      "ref": "origin/main",
+      "repo": "git@github.com:travisluong/fullstackbook-todo-nextjs.git",
+      "path": "/home/ubuntu/src",
+      "post-deploy": "source ~/.nvm/nvm.sh ; source ~/.profile ; npm install ; pm2 startOrRestart ecosystem.config.js",
+      "ssh_options": "ForwardAgent=yes"
+    }
+  }
+};
